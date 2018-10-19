0d867b45cbd8de64d6d62928e5f6ad92251e67f8
Christopher Chalmers <c.chalmers@me.com>
2018-10-19 20:05:52 +0100

    Add version of ihaskell that add the paths to the executables

---
+++ build-ihaskell.nix
@@ -0,0 +1,47 @@
+{ compiler ? "ghc843"
+, nixpkgs ? import <nixpkgs> {}
+, haskellPackages
+, packages ? (_: [])
+, pythonPackages ? (_: [])
+, rtsopts ? "-M3g -N2"
+, systemPackages ? (_: [])
+}:
+
+let
+  inherit (builtins) any elem filterSource listToAttrs;
+  lib = nixpkgs.lib;
+  cleanSource = name: type: let
+    baseName = baseNameOf (toString name);
+  in lib.cleanSourceFilter name type && !(
+    (type == "directory" && (elem baseName [ ".stack-work" "dist"])) ||
+    any (lib.flip lib.hasSuffix baseName) [ ".hi" ".ipynb" ".nix" ".sock" ".yaml" ".yml" ]
+  );
+  ihaskellSourceFilter = src: name: type: let
+    relPath = lib.removePrefix (toString src + "/") (toString name);
+  in cleanSource name type && ( any (lib.flip lib.hasPrefix relPath) [
+    "src" "main" "html" "Setup.hs" "ihaskell.cabal" "LICENSE"
+  ]);
+  ihaskellEnv = haskellPackages.ghcWithPackages (self: [ self.ihaskell self.ihaskell-diagrams ] ++ packages self);
+  notebook = nixpkgs.python3.withPackages (ps: [ ps.notebook ] ++ pythonPackages ps);
+  ihaskellSh = cmd: extraArgs: nixpkgs.writeScriptBin "ihaskell-${cmd}" ''
+    #! ${nixpkgs.stdenv.shell}
+    export GHC_PACKAGE_PATH="$(echo ${ihaskellEnv}/lib/*/package.conf.d| tr ' ' ':'):$GHC_PACKAGE_PATH"
+    export PATH="${nixpkgs.stdenv.lib.makeBinPath ([ ihaskellEnv notebook ] ++ systemPackages nixpkgs)}:$PATH"
+    ${ihaskellEnv}/bin/ihaskell install -l $(${ihaskellEnv}/bin/ghc --print-libdir) --use-rtsopts="${rtsopts}" && ${notebook}/bin/jupyter ${cmd} ${extraArgs} "$@"
+  '';
+in
+nixpkgs.buildEnv {
+  name = "ihaskell-with-packages";
+  buildInputs = [ nixpkgs.makeWrapper ];
+  paths = [ ihaskellEnv notebook ];
+  postBuild = ''
+    ln -s ${ihaskellSh "notebook" ""}/bin/ihaskell-notebook $out/bin/
+    ln -s ${ihaskellSh "nbconvert" ""}/bin/ihaskell-nbconvert $out/bin/
+    ln -s ${ihaskellSh "console" "--kernel=haskell"}/bin/ihaskell-console $out/bin/
+    for prg in $out/bin"/"*;do
+      if [[ -f $prg && -x $prg ]]; then
+        wrapProgram $prg --set PYTHONPATH "$(echo ${notebook}/lib/*/site-packages)"
+      fi
+    done
+  '';
+}
